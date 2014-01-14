16abcb5d2961c95ea2a57e7a85f66d88c4adbad8
Wade <sazhuo@cisco>
2014-01-14 09:21:53 +0800

    complete the adapter and activity

--- ListAndadapters/src/com/example/listandadapters/MainActivity.java
+++ ListAndadapters/src/com/example/listandadapters/MainActivity.java
@@ -1,19 +1,47 @@
 package com.example.listandadapters;
 
 import android.os.Bundle;
+import android.R.integer;
+import android.R.string;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.view.Menu;
+import android.widget.AbsListView;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
+	public int topVisiblePosition;
+	private TextView topHeader;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ListView mListView=(ListView) findViewById(R.id.list_view);
-        mListView.setEmptyView(findViewById(R.id.empty_view));
-        
+        setContentView(R.layout.list);
+        topHeader=(TextView) findViewById(R.id.header);
+//        ListView mListView=(ListView) findViewById(R.id.list_view);
+        setListAdapter(new SectionAdapter(this, Countries.COUNTRIES));
+//        mListView.setEmptyView(findViewById(R.id.empty_view));
+        getListView().setOnScrollListener(new AbsListView.OnScrollListener() {
+			
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {
+				// TODO Auto-generated method stub
+				if (firstVisibleItem!=topVisiblePosition) {
+					topVisiblePosition=firstVisibleItem;
+					setTopHeader(firstVisibleItem);
+				}
+			}
+		});
+        setTopHeader(0);
     }
 
 
@@ -23,5 +51,9 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+    private void setTopHeader(int pos)
+    {
+    	final String text= Countries.COUNTRIES[pos].substring(0,1);
+    	topHeader.setText(text);
+    }
 }
--- ListAndadapters/src/com/example/listandadapters/SectionAdapter.java
+++ ListAndadapters/src/com/example/listandadapters/SectionAdapter.java
@@ -5,21 +5,36 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class SectionAdapter extends ArrayAdapter<String> {
 
 	private Activity activity;
 
 	public SectionAdapter(Activity activity, String[] objects) {
-		super(activity, 0);
+		super(activity, R.layout.listitem, R.id.lable, objects);
+		this.activity = activity;
 		// TODO Auto-generated constructor stub
 		// return null;
 
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		return super.getView(position, convertView, parent);
+	public View getView(int position, View view, ViewGroup parent) {
+		if (view == null) {
+			view = activity.getLayoutInflater().inflate(R.layout.listitem,
+					parent, false);
+
+		}
+		TextView header = (TextView) view.findViewById(R.id.header);
+
+		String label = getItem(position);
+		if (position == 0 || getItem(position - 1).charAt(0) != label.charAt(0))
+		{
+			header.setVisibility(View.VISIBLE);
+			header.setText(label.substring(0, 1));
+		}
+		return super.getView(position, view, parent);
 	}
 
 }
