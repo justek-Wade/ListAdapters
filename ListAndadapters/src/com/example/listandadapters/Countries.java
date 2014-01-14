62c019c4ff36b590831a3b40041a19d05b2b2819
Wade <sazhuo@cisco>
2014-01-14 11:41:32 +0800

    add recycle type to getView. add some data in countries. change the background of header.

--- ListAndadapters/src/com/example/listandadapters/Countries.java
+++ ListAndadapters/src/com/example/listandadapters/Countries.java
@@ -3,5 +3,5 @@ package com.example.listandadapters;
 public class Countries {
 
 	public static String[] COUNTRIES = { "China", "Japan", "America",
-			"English", "Korea", "Germany", "Russia","Canada" };
+			"English", "Korea", "Germany", "Russia","Canada","9","9999","9","9999","9","9999","9","9999","17" ,"17","17","17","17"};
 }
--- ListAndadapters/src/com/example/listandadapters/SectionAdapter.java
+++ ListAndadapters/src/com/example/listandadapters/SectionAdapter.java
@@ -2,12 +2,19 @@ package com.example.listandadapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 public class SectionAdapter extends ArrayAdapter<String> {
+
+	static class ViewHolder {
+		public TextView header;
+		public TextView lableTextView;
+
+	}
 
 	private Activity activity;
 
@@ -21,20 +28,48 @@ public class SectionAdapter extends ArrayAdapter<String> {
 
 	@Override
 	public View getView(int position, View view, ViewGroup parent) {
+
+		// traditional type to getView
+		// if (view == null) {
+		// view = activity.getLayoutInflater().inflate(R.layout.listitem,
+		// parent, false);
+		//
+		// }
+		// TextView header = (TextView) view.findViewById(R.id.header);
+		//
+		// String label = getItem(position);
+		// if (position == 0 || getItem(position - 1).charAt(0) !=
+		// label.charAt(0))
+		// {
+		// header.setVisibility(View.VISIBLE);
+		// header.setText(label.substring(0, 1));
+		// }
+		// Log.v("getView", "traditional type to getView");
+		// return super.getView(position, view, parent);
+		
+		//========================================================================
+		//recycle type to getView
+		ViewHolder viewHolder;
 		if (view == null) {
+
 			view = activity.getLayoutInflater().inflate(R.layout.listitem,
 					parent, false);
-
+			viewHolder = new ViewHolder();
+			viewHolder.header = (TextView) view.findViewById(R.id.header);
+			viewHolder.lableTextView = (TextView) view.findViewById(R.id.lable);
+			view.setTag(viewHolder);
+		} else {
+			viewHolder = (ViewHolder) view.getTag();
+			viewHolder.header.setVisibility(View.GONE);
 		}
-		TextView header = (TextView) view.findViewById(R.id.header);
-
 		String label = getItem(position);
-		if (position == 0 || getItem(position - 1).charAt(0) != label.charAt(0))
-		{
-			header.setVisibility(View.VISIBLE);
-			header.setText(label.substring(0, 1));
+		if (position == 0 || getItem(position - 1).charAt(0) != label.charAt(0)) {
+			viewHolder.header.setVisibility(View.VISIBLE);
+			viewHolder.header.setText(label.substring(0, 1));
 		}
-		return super.getView(position, view, parent);
+		viewHolder.lableTextView.setText(label);
+		Log.v("getView", "recycle type to getView");
+		return view;
 	}
 
 }
